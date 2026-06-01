1ac3b632093efa85f62cc95cac41080d4af31c53
LukeMAmos <lukeamos2111@gmail.com>
2026-06-01 09:38:50 +0100

    Circular Buffer Implementation

--- Source/FourierTransformation.h
+++ Source/FourierTransformation.h
@@ -10,3 +10,78 @@
 
 #pragma once
 #include <JuceHeader.h>
+
+
+class CircBuff{
+    
+public:
+    
+    void prepare(double sampleRateIn, int bufferSamples , int channelIn ){
+        
+        sampleRate = sampleRateIn;
+        buffer.resize(bufferSamples, 0);
+        writePosition = 0;
+        channel = channelIn;
+        
+    }
+
+    void processSample(juce::AudioBuffer<float> &inputSamples){
+        
+        //Take the input samples and write over the oldest samples in the buffer
+        
+        auto samplesIn = inputSamples.getReadPointer(channel);
+        
+        for(int i = 0; i < inputSamples.getNumSamples() ; i++ ){
+            
+            buffer[(writePosition + i ) % buffer.size()] = samplesIn[i];
+
+        }
+        writePosition = (writePosition + inputSamples.getNumSamples()) % buffer.size();
+    }
+    
+    float bufferValue(int samplePos){
+        
+        return buffer[samplePos];
+    }
+    
+private:
+    
+    std::vector<float> buffer;
+    int writePosition;
+    double sampleRate;
+    int channel;
+    
+};
+
+
+class DFT{
+    
+public:
+    
+    void prepare();
+    
+    void setParameters();
+    
+    void process();
+
+    
+private:
+    
+};
+
+
+
+class FFT{
+    
+public:
+    
+    void prepare();
+    
+    void setParameters();
+    
+    void process();
+
+    
+private:
+    
+};
--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -9,7 +9,7 @@
 #pragma once
 
 #include <JuceHeader.h>
-#include <FourierTransformation.h>
+#include "FourierTransformation.h"
 
 //==============================================================================
 /**
