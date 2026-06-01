6cdd455beb684e42259353b28da3bd25a8e4acb1
LukeMAmos <lukeamos2111@gmail.com>
2026-06-01 11:23:13 +0100

    Implementing into the audio processor

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -56,5 +56,6 @@ public:
 
 private:
     //==============================================================================
+    FFT fourierTrans; 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (UsingFFTAudioProcessor)
 };
