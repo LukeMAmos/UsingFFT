b59c0d652fd1752c05479a6a3202473217125053
LukeMAmos <lukeamos2111@gmail.com>
2026-06-01 10:27:14 +0100

    Initial Implementation of FFT
    Additional Structure for holding complex values

--- Source/FourierTransformation.h
+++ Source/FourierTransformation.h
@@ -25,25 +25,25 @@ public:
         
     }
 
-    void processSample(juce::AudioBuffer<float> &inputSamples){
+    void processSample(juce::AudioBuffer<float> &inputBuffer){
         
         //Take the input samples and write over the oldest samples in the buffer
         
-        auto samplesIn = inputSamples.getReadPointer(channel);
+        auto samplesIn = inputBuffer.getReadPointer(channel);
         
-        for(int i = 0; i < inputSamples.getNumSamples() ; i++ ){
+        for(int i = 0; i < inputBuffer.getNumSamples() ; i++ ){
             
             buffer[(writePosition + i ) % buffer.size()] = samplesIn[i];
 
         }
-        writePosition = (writePosition + inputSamples.getNumSamples()) % buffer.size();
+        writePosition = (writePosition + inputBuffer.getNumSamples()) % buffer.size();
     }
     
     float orderedSamples(int positionInOrder){
         //getting the samples in order from oldest to newest no matter when in the circular buffer they are
         //The relationship of write position and oldestSample is writePos + 1 , so for an input of position we must return buffer [writepos +1 + position ]
         
-        return buffer[(writePosition + 1 + positionInOrder)%buffer.size()]; 
+        return buffer[(writePosition + 1 + positionInOrder)%buffer.size()];
     }
     
 private:
@@ -55,35 +55,87 @@ private:
     
 };
 
-
-class DFT{
+struct Complex{
     
-public:
-    
-    void prepare();
-    
-    void setParameters();
-    
-    void process();
-
-    
-private:
-    
+    float imaginary;
+    float real;
 };
 
 
+//Apply windowing functions in the DFT and FFT to keep the CircBuff clean
 
 class FFT{
     
 public:
     
-    void prepare();
+    void prepare(int FFTSize, float sampleRateIn){
+        
+        //Set up , including setting up the windowing function (Hann)
+        sampleRate = sampleRateIn;
+        fftSize = FFTSize;
+        windowing.resize(fftSize, 0.0f);
+        complexOut.resize(fftSize/2, {0.0f , 0.0f});
+        orderedBuffer.resize(fftSize, 0.0f);
+        
+        //Setting up the Hann values for use on multiplying
+        for(int i = 0 ; i < windowing.size() ; i ++){
+            
+            windowing[i] = 0.5 * (1 - std::cos((2 * M_PI) * i / (windowing.size()-1)));
+            
+        }
+        
+        //Only running a singlular left channel
+        circBuffer.prepare(sampleRate, fftSize, 0);
+    }
     
-    void setParameters();
     
-    void process();
+    void process(juce::AudioBuffer<float>& inputBuffer){
+        
+        circBuffer.processSample(inputBuffer); // write the new block of audio to the circular buffer.
+        
+        //order the data from circular buffer and multiply by the window value
+        for(int i = 0 ; i < orderedBuffer.size() ; i++){
+            
+            orderedBuffer[i] = circBuffer.orderedSamples(i) * windowing[i];
+        }
+        
+    }
+    
+    std::vector<Complex> calcFFT(std::vector<float> input){
+        
+        //Base case if the size of the input is 1 then return itself as a complex number
+        if(input.size() == 1 ){
+            
+            return { Complex{ input[0], 0.0f } };
+        }
+        
+        std::vector<float> even;
+        std::vector<float> odd;
+        
+        for(int i = 0 ; i < input.size() ; i ++ ){
+            
+            if(i % 2 == 0 ){ //is Even
+                
+                even.push_back(input[i]);
+            }else {
+                
+                odd.push_back(input[i]);
+            }
+        }
+        
+        std::vector<Complex> E = calcFFT(even);
+        std::vector<Complex> O = calcFFT(odd);
+        
+    }
 
     
 private:
     
+    CircBuff circBuffer;
+    std::vector<float> windowing;
+    std::vector<Complex> complexOut;
+    std::vector<float> orderedBuffer;
+    
+    int fftSize;
+    float sampleRate;
 };
